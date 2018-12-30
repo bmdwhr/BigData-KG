3a0dc9d67c5453bbb9534c9fa45421c4dddf1be1
bmdwhr <26445936@qq.com>
2018-12-30 12:06:07 +0800

    全国空气质量数据处理与可视化——pd+
    全国空气质量数据处理与可视化——pd+

---
+++ aqi_keshipd.py
@@ -0,0 +1,41 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.sans-serif'] = ['SimHei']#用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False#用来正常显示负号
+
+
+def main():
+    """
+        主函数
+    """
+    aqi_data = pd.read_csv('china_city_aqi.csv')
+    print('基本信息：')
+    print(aqi_data.info())
+
+    print('数据预览：')
+    print(aqi_data.head())
+
+    # 数据清洗
+    # 只保留AQI>0的数据
+    # filter_condition = aqi_data['AQI'] > 0
+    # clean_aqi_data = aqi_data[filter_condition]
+
+    clean_aqi_data = aqi_data[aqi_data['AQI'] > 0]
+
+    # 基本统计
+    print('AQI最大值:', clean_aqi_data['AQI'].max())
+    print('AQI最小值：', clean_aqi_data['AQI'].min())
+    print('AQI均值：', clean_aqi_data['AQI'].mean())
+
+    # top50数据可视化
+    top50_cities = clean_aqi_data.sort_values(by=['AQI']).head(50)
+    top50_cities.plot(kind='bar', x='City', y='AQI', title='空气质量最好的50个城市',
+                      figsize=(20, 10))#kind图形，figsize图片大小
+    plt.savefig('top50_aqi_bar.png')#保存
+    plt.show()#显示
+
+
+if __name__ == '__main__':
+    main()
